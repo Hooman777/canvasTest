76c1086be750257ff739c5664ad3e98ce92cff66
Seyedhooman Hesamyan <seyedhooman_hesamyan@edu.aua.am>
2017-10-17 10:19:39 +0400

    modified for concerning collisions of two balls

--- canvas.js
+++ canvas.js
@@ -78,25 +78,55 @@ animator();
 // }
 //  animator (100, 100);
 //**********************************************************************************
-var x = 500;
+var x = 200;
 var y = 100;
-var difx = 3;
-var dify = 3;
+var x2 = 300;
+var y2 = 300;
+var difx = 7;
+var dify = 7;
+var difx2 = 8;
+var dify2 = 8;
 const animate = function () {
     requestAnimationFrame (animate);
     c.clearRect (0, 0, innerWidth, innerHeight);
     c.beginPath ();
     c.arc (x, y, 35, 0, Math.PI * 2, false);
+	c.fillStyle = 'blue';
     c.strokeStyle = 'blue';
-    c.stroke ();
-    if (x - 35 >= innerWidth || (x - 35) <= 0) {
+    //c.stroke ();
+	c.fill ();
+    if ((x + 35) >= innerWidth || (x - 35) <= 0) {
         difx = - difx;
     }
-    if (y - 35 >= innerHeight || (y - 35) <= 0) {
-        difx = - difx;
+    if ((y + 35) >= innerHeight || (y - 35) <= 0) {
+        dify = - dify;
     }
     x = x + difx;
-    y = x + dify;
-
+    y = y + dify;
+	//************
+	c.beginPath ();
+    c.arc (x2, y2, 30, 0, Math.PI * 2, false);
+	c.fillStyle = 'green';
+    c.strokeStyle = 'blue';
+    //c.stroke ();
+	c.fill ();
+	var distance = Math.pow((Math.pow((x-x2),2) + Math.pow((y-y2),2)),0.5);
+	if (distance <= (30+35)) {
+		difx = - difx;
+		dify = - dify;
+		difx2 = - difx2;
+		dify2 = - dify2;
+	}
+    if ((x2 + 30) >= innerWidth || (x2 - 30) <= 0) {
+        difx2 = - difx2;
+    }
+    if ((y2 + 30) >= innerHeight || (y2 - 30) <= 0) {
+        dify2 = - dify2;
+    }
+    x2 = x2 + difx2;
+    y2 = y2 + dify2;
+	console.log (distance);
 }
 animate ();
+
+
